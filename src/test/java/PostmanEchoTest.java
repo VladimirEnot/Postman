b286133846f35fa5c2892c6df341234dab77a8cc
VladimirEnot <105981763+VladimirEnot@users.noreply.github.com>
2023-08-09 21:11:20 +0300

    add test +bag

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class PostmanEchoTest {
+    @Test
+    void ReturnOfData() {
+
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hijjj"));
+        ;
+    }
 }
+
+
